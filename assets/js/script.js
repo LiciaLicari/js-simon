2c94bd9125ad1e000049837d0f8cbf867575c793
“LiciaLicari” <licianbsplicari@gmail.com>
2023-09-05 14:18:44 +0200

    Initial Commit

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+console.log("Miao");
+
+/*
+
+Traccia
+
+Ogni secondo il nostro countdown dovrà scalare fino alle 9:30 di domani mattina!
+Consigli
+Questo esercizio richiede un po' di ricerca ma anche un po' di domande che accompagnano l'uomo 
+da tempo immemore:
+Da quante ore è composto un giorno? 1g ---> 24h
+Da quanti minuti è composta un'ora? 1h ---> 60m
+Da quanti secondi è composto un minuto?1m ---> 60s
+Da quanti millisecondi è composto un secondo? 1s ---> 1000ms
+Quanti millisecondi mi separano da domani alle 9:30?
+Esiste un oggetto JS in grado di gestire le date? new date
+Esistono dei metodi per trasformare una data in millisecondi?
+
+*/
+
+// const d = new Date("September 06, 2023 09:30:00");
+// let ms = d.getMilliseconds();
